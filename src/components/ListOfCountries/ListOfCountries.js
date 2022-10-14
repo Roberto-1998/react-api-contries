1121bf924656cc22de6b6af9142f36042ae1b145
Roberto <rcastellong98@gmail.com>
2022-10-14 17:39:51 -0400

    [add] Searching by region ready

--- src/components/ListOfCountries/ListOfCountries.js
+++ src/components/ListOfCountries/ListOfCountries.js
@@ -7,25 +7,71 @@ const ListOfCountries = () => {
   const ref = useRef(null);
   const [searchText, setSearchText] = useState('');
   const { data: countries = [], isError, isLoading } = useGetCountries();
+  const [region, setRegion] = useState('All');
+  console.log(
+    '🚀 ~ file: ListOfCountries.js ~ line 11 ~ ListOfCountries ~ region',
+    region,
+  );
 
   const handleSearch = () => {
     setSearchText(ref.current.value);
   };
 
-  const filteredCountries = useMemo(
-    () =>
-      countries.filter((country) => {
-        return country.name.common
-          .toLowerCase()
-          .includes(searchText.toLowerCase());
-      }),
-    [searchText],
-  );
+  const filteredCountries = useMemo(() => {
+    /*  switch (region) {
+      case region !== 'All':
+        switch (searchText) {
+          case searchText !== '':
+            return countries
+              .filter((country) =>
+                country.name.common
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase()),
+              )
+              .filter((country) => country.region === region);
 
-  const initialCountriesList = useMemo(
-    () => (searchText !== '' ? filteredCountries : countries),
-    [filteredCountries, countries, searchText],
-  );
+          case searchText === '':
+            return countries.filter((country) => country.region === region);
+        }
+        break;
+
+      case region === 'All':
+        switch (searchText) {
+          case searchText !== '':
+            return countries.filter((country) =>
+              country.name.common
+                .toLowerCase()
+                .includes(searchText.toLowerCase()),
+            );
+
+          case searchText === '':
+            return countries;
+        }
+        break;
+    } */
+
+    if (region !== 'All') {
+      if (searchText !== '') {
+        return countries
+          .filter((country) =>
+            country.name.common
+              .toLowerCase()
+              .includes(searchText.toLowerCase()),
+          )
+          .filter((country) => country.region === region);
+      }
+      return countries.filter((country) => country.region === region);
+    }
+
+    if (region === 'All') {
+      if (searchText !== '') {
+        return countries.filter((country) =>
+          country.name.common.toLowerCase().includes(searchText.toLowerCase()),
+        );
+      }
+      return countries;
+    }
+  }, [searchText, region, countries]);
 
   if (isError) return <h4>Ups..something went wrong</h4>;
 
@@ -34,8 +80,16 @@ const ListOfCountries = () => {
   return (
     <>
       <input type='text' value={searchText} onChange={handleSearch} ref={ref} />
+
+      <select value={region} onChange={(e) => setRegion(e.target.value)}>
+        <option value='All'>All</option>
+        <option value='Americas'>Americas</option>
+        <option value='Africa'>Africa</option>
+        <option value='Asia'>Asia</option>
+        <option value='Europe'>Europa</option>
+      </select>
       <List>
-        {initialCountriesList.map((country) => (
+        {filteredCountries.map((country) => (
           <Country key={country.id} {...country}></Country>
         ))}
       </List>
