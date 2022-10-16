c6241dda299304c23ab273e22bcc1e62fbbd7256
Roberto <rcastellong98@gmail.com>
2022-10-16 12:38:08 -0400

    [bugfix]

--- src/components/SelectRegion/SelectRegion.js
+++ src/components/SelectRegion/SelectRegion.js
@@ -3,7 +3,11 @@ import { Select } from './styles';
 
 const SelectRegion = ({ region, selectOptions, setRegion }) => {
   return (
-    <Select value={region} onChange={(e) => setRegion(e.target.value)}>
+    <Select
+      name='regions'
+      value={region}
+      onChange={(e) => setRegion(e.target.value)}
+    >
       {selectOptions.map((option) => (
         <option value={option.value} key={option.value}>
           {option.text}
